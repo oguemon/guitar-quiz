19d025cfc27f036ea3c9f2c1033d3ce294d833c3
Ogura Katsuya <contact@oguemon.com>
2025-02-11 10:47:55 +0900

    TestとLinter/Formatterの設定を追加

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  tabWidth: 2,
+  trailingComma: 'es5',
+  singleQuote: true,
+};
+
+export default config;
