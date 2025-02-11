114f3549833b6a99ebd983e78eb03a44b8719b40
Ogura Katsuya <contact@oguemon.com>
2025-02-11 22:09:44 +0900

    結果画面のデザインを調整

--- src/components/QuizSection/ResultPanel/ResultPanel.tsx
+++ src/components/QuizSection/ResultPanel/ResultPanel.tsx
@@ -11,23 +11,39 @@ export const ResultPanel: FC<Props> = ({ history, totalCount }) => {
   const correctRate = Math.floor((correctCount / totalCount) * 100);
   return (
     <div className={styles.module}>
-      <h2>お疲れ様でした！</h2>
-      <ul>
-        <li>
-          正解率：<span>{correctRate}</span>%
-        </li>
-        <li>
-          <span>{totalCount}</span>問中
-          <span>{correctCount}</span>問正解
-        </li>
-      </ul>
-      <ul>
-        {history.map((correct, index) => (
-          <li>
-            第{index + 1}問：{correct ? '⭕️' : '❌'}
-          </li>
-        ))}
-      </ul>
+      <h2 className={styles.title}>結果発表</h2>
+      <p className={styles.message}>お疲れ様でした！</p>
+      <div>
+        <div className={styles.resultItem}>
+          <div className={styles.label}>正解率</div>
+          <div>
+            <span className={styles.resultValue}>{correctRate}</span>%
+          </div>
+        </div>
+        <div className={styles.resultItem}>
+          <div className={styles.label}>正解数</div>
+          <div>
+            <span className={styles.resultValue}>{correctCount}</span>
+            問正解／全{totalCount}問
+          </div>
+        </div>
+      </div>
+      <div>
+        <table>
+          <tr className={styles.detailTableLine}>
+            <th className={styles.detailTableHeaderCell}>問題</th>
+            <th className={styles.detailTableHeaderCell}>正誤</th>
+          </tr>
+          {history.map((correct, index) => (
+            <tr className={styles.detailTableLine}>
+              <td className={styles.detailTableBodyCell}>第{index + 1}問</td>
+              <td className={styles.detailTableBodyCell}>
+                {correct ? '⭕️' : '❌'}
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
     </div>
   );
 };
