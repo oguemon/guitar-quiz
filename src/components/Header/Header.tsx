5863b5a48fb9b1c15149651b9476de2a8e26ad89
Ogura Katsuya <contact@oguemon.com>
2025-02-12 23:09:12 +0900

    ヘッダーロゴの設定

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,11 @@
 import { FC } from 'react';
+import Logo from '../../assets/logo.svg';
 import styles from './Header.module.css';
 
 export const Header: FC = () => {
-  return <header className={styles.module}>ギター指板クイズくん</header>;
+  return (
+    <header className={styles.module}>
+      <img className={styles.logo} src={Logo} alt="ギター指板→音名クイズくん" />
+    </header>
+  );
 };
